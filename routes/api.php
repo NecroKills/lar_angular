9895e84d3876f25b4912f1a6b41cee1b058084a6
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-04-28 19:37:27 -0300

    configurado o env. configurado a route/api. criado a migration, o controlador e o model. O controlar já tem funções criadas com o --resource

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', 'PostControlador@index');
+
+Route::get('/', 'PostControlador@store');
+
+Route::delete('/{id}', 'PostControlador@destroy');
+
+Route::get('/lik/{id}', 'PostControlador@linke');
