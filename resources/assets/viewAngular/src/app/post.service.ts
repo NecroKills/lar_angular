ed30176e8fb8a74cc660700f683132263fe015ba
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-04-29 02:26:07 -0300

    a parte de buscar posts da api esta pronta, feito no arquivo post.service

--- resources/assets/viewAngular/src/app/post.service.ts
+++ resources/assets/viewAngular/src/app/post.service.ts
@@ -5,16 +5,18 @@ import { Post } from './post';
 @Injectable()
 export class PostService {
   
-    public posts: Post[] = [
-    new Post ("Joao", "Meu post", "Sub Joao", "joao@gmail.com", "minha msg"),
-    new Post ("Laiza", "Meu post", "Sub Laiza", "Laiza@gmail.com", "minha msg"),
-    new Post ("Maycon", "Meu post", "Sub Maycon", "Maycon@gmail.com", "minha msg"),
-    new Post ("Joao", "Meu post", "Sub Joao", "joao@gmail.com", "minha msg"),
-    new Post ("Laiza", "Meu post", "Sub Laiza", "Laiza@gmail.com", "minha msg"),
-    new Post ("Maycon", "Meu post", "Sub Maycon", "Maycon@gmail.com", "minha msg"),
-    new Post ("Joao", "Meu post", "Sub Joao", "joao@gmail.com", "minha msg"),
-    new Post ("Laiza", "Meu post", "Sub Laiza", "Laiza@gmail.com", "minha msg"),
-    new Post ("Maycon", "Meu post", "Sub Maycon", "Maycon@gmail.com", "minha msg"),
-  ];
-  constructor(HTTP: HttpClient) { }
+    public posts: Post[] = [];
+
+  //metodo construtor que recebe via construtor que vai retornar todos os posts do servidor
+  constructor(private http: HttpClient) {
+    this.http.get("/api/").subscribe(
+      (posts: any[]) => {
+        for(let p of posts){
+          this.posts.push(
+            new Post(p.nome,p.titulo, p.butitulo, p.email, p.mensagem, p.arquivo, p.id, p.likes)
+          );
+        }
+      }
+    );
+   }
 }
